5968ab5b359c176819c32e7d457bf0f35c67fbab
Gian Carlo Pace <giancarlo.pace@gmail.com>
2011-10-30 09:58:24 +0100

    First import of a rails app to train people in english lessons

---
+++ english_course/english_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EnglishApp::Application.config.secret_token = 'dd5b941a5bd96ac9b2098585a5484d7e7393930db779b5579b60215b6cc4e4250e118851fc1041507c9a80ac292e8a1bae09f1de2a4c335070dec760c6bc90e0'
